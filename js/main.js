e84dac30b93ac1e47c018b3d7a5e76c5e644bccd
Lucy Gow <lucy@Ians-MacBook-Pro.local>
2017-10-07 13:22:31 +1100

    Changed to white background. Split animations of captions and images.

--- js/main.js
+++ js/main.js
@@ -1,44 +1,132 @@
 $(document).ready(function(){
   var controller = new ScrollMagic.Controller();
 
-  var scene_title = new ScrollMagic.Scene({
-      triggerElement: '#title'
+  /*var scene_title = new ScrollMagic.Scene({
+      triggerElement: '#title',
+      duration:"130%",
+      triggerHook:0.1,
   }).setClassToggle('#title', 'fade-in')
+  .addIndicators({
+    name: 'fade title',
+    colorTrigger: 'black',
+    colorStart: '#75C695',
+    colorEnd: 'pink'
+  })
+    .addTo(controller);*/
+
+  var scene_1_caption = new ScrollMagic.Scene({
+      triggerElement: '#page1',
+      duration:"20%",
+      triggerHook:0.1,
+
+  }).setClassToggle('#page1>.caption', 'fade-in')
     .addTo(controller);
 
-  var scene_1 = new ScrollMagic.Scene({
-      triggerElement: '#page1'
-  }).setClassToggle('#page1', 'fade-in')
+  var scene_1_img = new ScrollMagic.Scene({
+      triggerElement: '#page1',
+      duration:"130%",
+      triggerHook:0.6,
+
+  }).setClassToggle('#page1>img', 'fade-in')
     .addTo(controller);
 
-  var scene_2 = new ScrollMagic.Scene({
-      triggerElement: '#page2'
-  }).setClassToggle('#page2', 'fade-in')
+  var scene_2_caption = new ScrollMagic.Scene({
+      triggerElement: '#page2',
+      duration:"20%",
+      triggerHook:0.1,
+
+  }).setClassToggle('#page2>.caption', 'fade-in')
     .addTo(controller);
 
-  var scene_3 = new ScrollMagic.Scene({
-      triggerElement: '#page3'
-  }).setClassToggle('#page3', 'fade-in')
+  var scene_2_img = new ScrollMagic.Scene({
+      triggerElement: '#page2',
+      duration:"130%",
+      triggerHook:0.6,
+
+  }).setClassToggle('#page2>img', 'fade-in')
     .addTo(controller);
 
-  var scene_4 = new ScrollMagic.Scene({
-      triggerElement: '#page4'
-  }).setClassToggle('#page4', 'fade-in')
+
+  var scene_3_caption = new ScrollMagic.Scene({
+      triggerElement: '#page3',
+      duration:"20%",
+      triggerHook:0.1,
+
+  }).setClassToggle('#page3>.caption', 'fade-in')
     .addTo(controller);
 
-  var scene_5 = new ScrollMagic.Scene({
-      triggerElement: '#page5'
-  }).setClassToggle('#page5', 'fade-in')
+  var scene_3_img = new ScrollMagic.Scene({
+      triggerElement: '#page3',
+      duration:"130%",
+      triggerHook:0.6,
+
+  }).setClassToggle('#page3>img', 'fade-in')
     .addTo(controller);
 
-  var scene_6 = new ScrollMagic.Scene({
-      triggerElement: '#page6'
-  }).setClassToggle('#page6', 'fade-in')
+  var scene_4_caption = new ScrollMagic.Scene({
+      triggerElement: '#page4',
+      duration:"20%",
+      triggerHook:0.1,
+
+  }).setClassToggle('#page4>.caption', 'fade-in')
     .addTo(controller);
 
-  var scene_7 = new ScrollMagic.Scene({
-      triggerElement: '#page7'
-  }).setClassToggle('#page7', 'fade-in')
+  var scene_4_img = new ScrollMagic.Scene({
+      triggerElement: '#page4',
+      duration:"130%",
+      triggerHook:0.6,
+
+  }).setClassToggle('#page4>img', 'fade-in')
+    .addTo(controller);
+
+
+  var scene_5_caption = new ScrollMagic.Scene({
+      triggerElement: '#page5',
+      duration:"20%",
+      triggerHook:0.1,
+
+  }).setClassToggle('#page5>.caption', 'fade-in')
+    .addTo(controller);
+
+  var scene_5_img = new ScrollMagic.Scene({
+      triggerElement: '#page5',
+      duration:"130%",
+      triggerHook:0.6,
+
+  }).setClassToggle('#page5>img', 'fade-in')
+    .addTo(controller);
+
+  var scene_6_caption = new ScrollMagic.Scene({
+      triggerElement: '#page6',
+      duration:"20%",
+      triggerHook:0.1,
+
+  }).setClassToggle('#page6>.caption', 'fade-in')
+    .addTo(controller);
+
+  var scene_6_img = new ScrollMagic.Scene({
+      triggerElement: '#page6',
+      duration:"130%",
+      triggerHook:0.6,
+
+  }).setClassToggle('#page6>img', 'fade-in')
+    .addTo(controller);
+
+
+  var scene_7_caption = new ScrollMagic.Scene({
+      triggerElement: '#page7',
+      duration:"20%",
+      triggerHook:0.1,
+
+  }).setClassToggle('#page7>.caption', 'fade-in')
+    .addTo(controller);
+
+  var scene_7_img = new ScrollMagic.Scene({
+      triggerElement: '#page7',
+      duration:"130%",
+      triggerHook:0.6,
+
+  }).setClassToggle('#page7>img', 'fade-in')
     .addTo(controller);
 
 });
