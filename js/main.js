21354edc51966405b944921a77cc523c6dd5a6d4
Lucy Gow <lucy@Ians-MacBook-Pro.local>
2017-10-09 14:15:20 +1100

    Refactored.

--- js/main.js
+++ js/main.js
@@ -1,137 +1,28 @@
 $(document).ready(function(){
-  $("img.lazy").each(function() {
+  $("img.lazy ").each(function() {
     $(this).attr("src", $(this).attr("data-original"));
     $(this).removeAttr("data-original");
 });
 
   var controller = new ScrollMagic.Controller();
 
-  /*var scene_title = new ScrollMagic.Scene({
-      triggerElement: '#title',
-      duration:"110%",
-      triggerHook:0.5,
-  }).setClassToggle('#title', 'fade-in')
-  .addIndicators({
-    name: 'fade title',
-    colorTrigger: 'black',
-    colorStart: '#75C695',
-    colorEnd: 'pink'
-  })
-    .addTo(controller);*/
-
-  var scene_1_caption = new ScrollMagic.Scene({
-      triggerElement: '#page1',
+  $('figure').each(function(){
+    
+    var caption_scene = new ScrollMagic.Scene({
+      triggerElement: this,
       duration:"60%",
       triggerHook:0.5,
 
-  }).setClassToggle('#page1>figcaption', 'fade-in')
+  }).setClassToggle($(this).children('figcaption')[0], 'fade-in')
     .addTo(controller);
 
-  var scene_1_img = new ScrollMagic.Scene({
-      triggerElement: '#page1',
+  var img_scene = new ScrollMagic.Scene({
+      triggerElement: this,
       duration:"110%",
       triggerHook:0.7,
 
-  }).setClassToggle('#page1>img', 'fade-in')
+  }).setClassToggle($(this).children('img')[0], 'fade-in')
     .addTo(controller);
-
-  var scene_2_caption = new ScrollMagic.Scene({
-      triggerElement: '#page2',
-      duration:"60%",
-      triggerHook:0.5,
-
-  }).setClassToggle('#page2>figcaption', 'fade-in')
-    .addTo(controller);
-
-  var scene_2_img = new ScrollMagic.Scene({
-      triggerElement: '#page2',
-      duration:"110%",
-      triggerHook:0.7,
-
-  }).setClassToggle('#page2>img', 'fade-in')
-    .addTo(controller);
-
-
-  var scene_3_caption = new ScrollMagic.Scene({
-      triggerElement: '#page3',
-      duration:"60%",
-      triggerHook:0.5,
-
-  }).setClassToggle('#page3>figcaption', 'fade-in')
-    .addTo(controller);
-
-  var scene_3_img = new ScrollMagic.Scene({
-      triggerElement: '#page3',
-      duration:"110%",
-      triggerHook:0.7,
-
-  }).setClassToggle('#page3>img', 'fade-in')
-    .addTo(controller);
-
-  var scene_4_caption = new ScrollMagic.Scene({
-      triggerElement: '#page4',
-      duration:"60%",
-      triggerHook:0.5,
-
-  }).setClassToggle('#page4>figcaption', 'fade-in')
-    .addTo(controller);
-
-  var scene_4_img = new ScrollMagic.Scene({
-      triggerElement: '#page4',
-      duration:"110%",
-      triggerHook:0.7,
-
-  }).setClassToggle('#page4>img', 'fade-in')
-    .addTo(controller);
-
-
-  var scene_5_caption = new ScrollMagic.Scene({
-      triggerElement: '#page5',
-      duration:"60%",
-      triggerHook:0.5,
-
-  }).setClassToggle('#page5>figcaption', 'fade-in')
-    .addTo(controller);
-
-  var scene_5_img = new ScrollMagic.Scene({
-      triggerElement: '#page5',
-      duration:"110%",
-      triggerHook:0.7,
-
-  }).setClassToggle('#page5>img', 'fade-in')
-    .addTo(controller);
-
-  var scene_6_caption = new ScrollMagic.Scene({
-      triggerElement: '#page6',
-      duration:"60%",
-      triggerHook:0.5,
-
-  }).setClassToggle('#page6>figcaption', 'fade-in')
-    .addTo(controller);
-
-  var scene_6_img = new ScrollMagic.Scene({
-      triggerElement: '#page6',
-      duration:"110%",
-      triggerHook:0.7,
-
-  }).setClassToggle('#page6>img', 'fade-in')
-    .addTo(controller);
-
-
-  var scene_7_caption = new ScrollMagic.Scene({
-      triggerElement: '#page7',
-      duration:"60%",
-      triggerHook:0.5,
-
-  }).setClassToggle('#page7>figcaption', 'fade-in')
-    .addTo(controller);
-
-  var scene_7_img = new ScrollMagic.Scene({
-      triggerElement: '#page7',
-      duration:"110%",
-      triggerHook:0.7,
-
-  }).setClassToggle('#page7>img', 'fade-in')
-    .addTo(controller);
+  });
 
 });
