1c548bb0756f2eb3618eaabbbf562934301d195b
Lucy Gow <lucy@Ians-MacBook-Pro.local>
2017-10-10 12:19:12 +1100

    Added page 8.

--- js/main.js
+++ js/main.js
@@ -6,23 +6,24 @@ $(document).ready(function(){
 
   var controller = new ScrollMagic.Controller();
 
-  $('figure').each(function(){
+    $('figure').each(function(){
     
-    var caption_scene = new ScrollMagic.Scene({
-      triggerElement: this,
-      duration:"60%",
-      triggerHook:0.5,
+      var caption_scene = new ScrollMagic.Scene({
+        triggerElement: this,
+        duration:"60%",
+        triggerHook:0.5,
 
-  }).setClassToggle($(this).children('figcaption')[0], 'fade-in')
-    .addTo(controller);
+    })
+      .setClassToggle($(this).children('figcaption')[0], 'fade-in')
+      .addTo(controller);
 
-  var img_scene = new ScrollMagic.Scene({
-      triggerElement: this,
-      duration:"110%",
-      triggerHook:0.7,
-
-  }).setClassToggle($(this).children('img')[0], 'fade-in')
-    .addTo(controller);
+      var img_scene = new ScrollMagic.Scene({
+        triggerElement: this,
+        duration:"110%",
+        triggerHook:0.7,
+    })
+      .setClassToggle($(this).children('img')[0], 'fade-in')
+   //   .setPin($(this).prev().children('img')[0], {pushFollowers:false})
+      .addTo(controller);
   });
-
 });
